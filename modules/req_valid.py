a2b3aa4a0a7a877631af0c7c5822544278b57d2d
namth <wase894@gmail.com>
2021-12-30 08:09:59 +0900

    api_key만 넘겨주도록 변경

--- modules/req_valid.py
+++ modules/req_valid.py
@@ -1,9 +1,9 @@
 import functools
 from flask import Response
 import os
-from cryptography.hazmat.primitives.serialization import load_pem_private_key
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric.padding import MGF1, OAEP
+# from cryptography.hazmat.primitives.serialization import load_pem_private_key
+# from cryptography.hazmat.primitives import hashes
+# from cryptography.hazmat.primitives.asymmetric.padding import MGF1, OAEP
 
 
 def health_cors(func):
@@ -39,18 +39,18 @@ def auth_deco(func):
             if(key_type.lower() != "bearer"):
                 raise ValueError
             # 암호 불러오기
-            private_key_str = os.environ.get('private', None).encode("utf-8")
-            secret = os.environ.get("secret", None).encode("utf-8")
+            # private_key_str = os.environ.get('private', None).encode("utf-8")
+            # secret = os.environ.get("secret", None).encode("utf-8")
             env_api_key = os.environ.get("api_key", None)
 
             # 비공개 키 로드
-            padding = OAEP(mgf=MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None)
-            private_key = load_pem_private_key(private_key_str, password=secret)
+            # padding = OAEP(mgf=MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None)
+            # private_key = load_pem_private_key(private_key_str, password=secret)
 
             # 복호화
-            decrypted = private_key.decrypt(req_api_key.encode("utf-8"), padding).decode("utf-8")
+            # decrypted = private_key.decrypt(req_api_key.encode("utf-8"), padding).decode("utf-8")
             # 검사
-            if(decrypted != env_api_key):
+            if(req_api_key != env_api_key):
                 raise ValueError
         except ValueError:
             return Response(status=401, response="Authorization is not correct format", headers=headers)
