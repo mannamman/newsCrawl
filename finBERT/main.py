3865520b172de815288c9726a84f6a97122587bb
namth <wase894@gmail.com>
2022-01-21 16:53:44 +0900

    호출 경로 수정

--- finBERT/main.py
+++ finBERT/main.py
@@ -1,4 +1,4 @@
-from finbert.finbert import predict
+from finBERT.finbert.finbert import predict
 from transformers import AutoModelForSequenceClassification
 import os
 
@@ -7,7 +7,8 @@ import os
 
 class FinBert:
     def __init__(self):
-        path = "models/sentiment"
+        # 실행한 디렉토리 기준으로 상대경로를 입력
+        path = "finBERT/models/sentiment"
         self.model = AutoModelForSequenceClassification.from_pretrained(path, num_labels=3,cache_dir=None)
     def pred(self, text):
         res = predict(text, self.model).to_json(orient='records')
