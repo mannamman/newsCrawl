74ff0c46a9d046568b55c8df08a4d6c19f487e90
namth <wase894@gmail.com>
2022-01-24 16:24:05 +0900

    도커파일 수정, 의존성 파일 업데이트, finbert파일 이름 수정

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
 FROM python:3.8.12-slim-buster
 LABEL maintainer="wase894@gmail.com"
+ENV TRANSFORMERS_OFFLINE 1 
 EXPOSE 8080
 WORKDIR /app
 COPY . /app
 RUN pip3 install -r requirements.txt
 RUN python3 -m nltk.downloader punkt -d /usr/local/nltk_data
 RUN python3 -m nltk.downloader stopwords -d /usr/local/nltk_data
-# 새로운 링크(finbert)로 변경 예정
-RUN gdown https://drive.google.com/uc?id=1EfeFNm6KvGB8z3uvRFRLDsm2tvqamA60 -O /app/sentiment_dir/pytorch_model.bin
-CMD exec gunicorn --bind 0.0.0.0:8080 --workers 1 --threads 1 --timeout 240 main:app
+# finbert
+RUN gdown https://drive.google.com/uc?id=1frgtv2KYvoega8IrL4PGlA8lAO_dh9If -O /app/finBERT/models/sentiment/pytorch_model.bin
+# CMD exec gunicorn --bind 0.0.0.0:8080 --workers 1 --threads 1 --timeout 240 main:app
---
+++ finBERT/sentiment.py
@@ -0,0 +1,33 @@
+from finBERT.finbert.finbert import predict
+from transformers import AutoModelForSequenceClassification
+import os
+import json
+
+
+
+
+class FinBert:
+    def __init__(self):
+        # 실행한 디렉토리 기준으로 상대경로를 입력
+        self.path = "finBERT/models/sentiment"
+        self.model = AutoModelForSequenceClassification.from_pretrained(self.path, num_labels=3,cache_dir=None)
+        self.labels = ["positive", "negative", "neutral"]
+
+    def _make_dic(self, text, logit):
+        return_dic = {
+            "sentence" : text,
+        }
+        for label, val in zip(self.labels, logit):
+            return_dic[label] = val
+        return return_dic
+
+    def pred(self, text):
+        print(f"{text} start ...")
+        res = json.loads(predict(text, self.model, self.path).to_json(orient='records'))[0]
+        print(f"{text} end ...")
+        return self._make_dic(text, res["logit"])
+
+
+if(__name__ == "__main__"):
+    fin = FinBert()
+    fin.pred("Inflation at 40-year high pressures consumers, Fed and Biden")
