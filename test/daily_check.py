e55e3096ff0528f030582007ae3ae35d8e9a2c06
namth <wase894@gmail.com>
2022-03-25 14:51:40 +0900

    :fire: remove: 불필요한 파일 삭제
    더 이상 사용치 않는 파일들 삭제

--- test/daily_check.py
+++
@@ -1,9 +0,0 @@
-from mongo_test import DBworker
-
-if(__name__ == "__main__"):
-    subjects = ["tesla", "snp500", "google"]
-    for subject in subjects:
-        worker = DBworker(subject)
-        print(f"{subject=}")
-        worker.test_find_all()
-        print("="*30)
